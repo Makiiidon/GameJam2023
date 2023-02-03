35a98e2a19abfb464825ba9cbf9840d7981fdc74
EthanM0113 <102124492+EthanM0113@users.noreply.github.com>
2023-02-03 22:38:18 +0800

    Shooting!

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour
 {
     InputHandler input;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float shotSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +17,18 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (input.GetShoot())
+        {
+            Debug.Log("Shooting!");
+            GameObject bulletShot = Instantiate(bullet, transform.position, Quaternion.identity);
+            Rigidbody2D bulletShotRb = bulletShot.GetComponent<Rigidbody2D>();
+            bulletShotRb.AddForce(new Vector2(shotSpeed, 0));
+
+        }
     }
 
     private void FixedUpdate()
     {
-        if(input.GetShoot())
-        {
-            Debug.Log("Shooting!");
-        }
+        
     }
 }
