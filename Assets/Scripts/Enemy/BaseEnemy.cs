a1df4a050d143b0cf61141f74f582eb8205822d0
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2023-02-04 00:39:25 +0800

    Added features
    Test Enemy
    Attacking
    Damaging enemies

---
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class BaseEnemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] protected int health = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerMelee.cs
+++ Assets/Scripts/PlayerMelee.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMelee : MonoBehaviour
 {
     InputHandler inputHandler;
+    PlayerController controller;
 
-    [SerializeField] private Vector3 attackPoint;
+    [SerializeField] int attackDamage = 2;
+
+    [SerializeField] private Transform attackPoint;
     [SerializeField] private float attackRadius = 2.5f;
     [SerializeField] private LayerMask whatIsEnemy;
 
@@ -16,18 +19,41 @@ public class PlayerMelee : MonoBehaviour
     void Start()
     {
         inputHandler = InputHandler.Instance;
+        controller = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float attackPos = attackPoint.position.x;
+
+        if (controller.IsFacingLeft())
+        {
+            attackPos = -0.798f + transform.position.x;
+        }
+        else if (!controller.IsFacingLeft())
+        {
+            attackPos = 0.798f + transform.position.x;
+        }
+
+        attackPoint.position = new Vector3(attackPos, transform.position.y, transform.position.z);
+
+
         if (inputHandler.GetAttack())
         {
-            if (Physics2D.OverlapCircle(attackPoint + transform.position, attackRadius, whatIsEnemy))
+            Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, whatIsEnemy);
+            Debug.Log("Atack");
+            foreach (Collider2D enemy in enemies)
             {
-                Debug.Log("Atack");
-
+                enemy.GetComponent<BaseEnemy>().TakeDamage(attackDamage);
             }
+
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
     }
 }
