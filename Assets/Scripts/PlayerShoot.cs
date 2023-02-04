d1fc862f0fd17ab5d1d3c57a9a8d1f1afb59760b
MLGalura <marc_lawrence_c_galura@dlsu.edu.ph>
2023-02-04 17:48:36 +0800

    Ammo Reload Pickup

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -24,7 +24,8 @@ public class PlayerShoot : MonoBehaviour
         currentAmmo = maxAmmo;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Reload"))
         {
