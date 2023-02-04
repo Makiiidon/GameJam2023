84d5fc5d392586b35e1b4a9c955fa38258fce70b
MLGalura <marc_lawrence_c_galura@dlsu.edu.ph>
2023-02-04 10:36:18 +0800

    Added Acorn bullet and rotation

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -16,6 +16,7 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private int addedAmmo = 5;
     [SerializeField] private float bulletAge = 0.5f;
 
+
     // Start is called before the first frame update
     void Start()
     {
